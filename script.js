06e02f2085c86375323d203f683e154c675e878a
aarontalua2 <aarontalua14@gmail.com>
2023-04-12 03:56:02 +0800

    Started project layout

---
+++ script.js
@@ -0,0 +1,25 @@
+// Operations
+const add = function (a, b) {
+	return a + b;
+};
+const subtract = function (a, b) {
+	return a - b;
+};
+const multiply = function (a, b) {
+	return a * b;
+};
+const divide = function (a, b) {
+	return a / b;
+};
+const operate = function (a, operator, b) {
+	switch (operator) {
+		case "+":
+			return add(+a, +b);
+		case "-":
+			return subtract(+a, +b);
+		case "*":
+			return multiply(+a, +b);
+		case "/":
+			return divide(+a, +b);
+	}
+};
